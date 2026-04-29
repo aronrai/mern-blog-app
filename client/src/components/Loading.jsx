70b591563fbf6c575ca137b33aba44e53f903a48
aron <aron.dumi.rai@gmail.com>
2026-04-29 17:29:59 +0530

    add loading component

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <p className="text-sm text-gray-500 font-heading tracking-widest uppercase animate-pulse">
+      LOADING...
+    </p>
+  );
+};
+
+export default Loading;
