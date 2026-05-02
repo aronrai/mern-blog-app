8d4152e17b123f9d9c24c0868c05818c7136dbd7
aron <aron.dumi.rai@gmail.com>
2026-05-02 11:15:47 +0530

    update nodemailer

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.NODEMAILER_USER,
     pass: process.env.NODEMAILER_PASS,
