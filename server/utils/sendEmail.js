970d79e33bfd50812433f10f2ffb2e1245e26e08
aron <aron.dumi.rai@gmail.com>
2026-05-02 15:12:52 +0530

    update nodemailer config

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,16 +2,11 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.NODEMAILER_USER,
     pass: process.env.NODEMAILER_PASS,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
 });
 
 const sendVerificationEmail = async (name, email, verificationToken) => {
