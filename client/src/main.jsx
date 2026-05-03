69d41a75ff7c320f721f25ace4b07d9196bea8ef
aron <aron.dumi.rai@gmail.com>
2026-05-03 16:10:15 +0530

    css import update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "@fontsource-variable/montserrat/wght.css";
-import "@fontsource/montez";
-import "@fontsource-variable/playfair-display/wght.css";
 import "./index.css";
 import App from "./App.jsx";
 
